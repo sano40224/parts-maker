fae446305e4b87f212d4000d048d1f691ce41512
sano40224 <yuuki130908@gmail.com>
2026-02-02 16:37:25 +0900

    Stop tracking front directory

--- front/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-
-const api = axios.create({
-  baseURL: BASE_URL,
-});
-
-export default api;
