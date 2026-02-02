f39d7a26fddb402eea0594bfa710f2584341590b
sano40224 <yuuki130908@gmail.com>
2026-02-03 02:20:45 +0900

    Update config for Render and add postgres driver

--- back/config.py
+++ back/config.py
@@ -2,9 +2,15 @@ import os
 
 
 class Config:
-    SECRET_KEY = 'dev-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/parts_maker_db?charset=utf8'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'mysql+pymysql://root:root@localhost/parts_maker_db?charset=utf8'
+
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SESSION_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SECURE = True
